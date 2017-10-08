b30d7a85d07f650339cc44b7d822c43402a1788c
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-08 01:41:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,6 @@ var array = [10,2,3];
 forLoop(array);
 function forLoop(array){
    for(let i=0; i<array.length;i++){
-     "I am ${i} strange loop${i === 0 ? '' : 's'}."
+     console.log("I am ${i} strange loop${i === 0 ? '' : 's'}.");
    }
  }
