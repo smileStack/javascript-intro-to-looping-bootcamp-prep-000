2848f9337802aff03468d679b448818837e30f1b
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-08 01:53:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,11 @@ var arr = [];
 forLoop(arr);
 function forLoop(array){
    for(let i=0; i<25;i++){
-     array[i] = "I am " + i +" strange loop"+ if(i>0){return 's'} + "."
+     if(i==0){
+       array[i] = "I am " + i +" strange loop."
+     }else{
+        array[i] = "I am " + i +" strange loops."
+     }
    }
    console.log(array);
    return array;
